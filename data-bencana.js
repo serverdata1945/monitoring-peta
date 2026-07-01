d442ce288e78dd40e60f93056e8949cd153ffa58
serverdata1945 <serverdata1945@gmail.com>
2026-07-01 12:35:50 +0700

    Add files via upload

---
+++ data-bencana.js
@@ -0,0 +1,27 @@
+// Tempat menyimpan data titik koordinat secara statis
+var laporan_array = [
+    {
+        "id": "1",
+        "kecamatan": "Kec. Ambulu",
+        "status": "Mendesak",
+        "latitude": -8.345600,
+        "longitude": 113.606100,
+        "keterangan": "Tanggul jebol, butuh bantuan karung pasir dan sembako darurat."
+    },
+    {
+        "id": "2",
+        "kecamatan": "Kec. Sumbersari",
+        "status": "Selesai",
+        "latitude": -8.184486,
+        "longitude": 113.668074,
+        "keterangan": "Genangan air sudah surut, bantuan logistik pakaian selesai disalurkan."
+    },
+    {
+        "id": "3",
+        "kecamatan": "Kec. Panti",
+        "status": "Verifikasi",
+        "latitude": -8.067300,
+        "longitude": 113.623400,
+        "keterangan": "Laporan longsor skala kecil di akses jalan desa, tim sedang menuju lokasi."
+    }
+];
